56f4a2fecc3cda68dd102976dee03f719be77738
Drew Owen <dcowen91@gmail.com>
2019-10-19 13:47:50 -0700

    scrape all positions, use request-promise-native instead of puppeteer, rewrite logic

--- scrape.js
+++ scrape.js
@@ -1,49 +1,47 @@
-const puppeteer = require("puppeteer");
+var rp = require("request-promise-native");
 
-(async () => {
-  const browser = await puppeteer.launch({ headless: false, slowMo: 250 });
-  const page = await browser.newPage();
+// TODO support PPR/HALF-PPR, NON-PPR etc
+// TODO by default just use PPR until build more sophisticated
+const Pages = {
+  QB: "https://s3-us-west-1.amazonaws.com/fftiers/out/text_QB.txt",
+  // RB: "https://s3-us-west-1.amazonaws.com/fftiers/out/text_RB.txt",
+  WRPPR: "https://s3-us-west-1.amazonaws.com/fftiers/out/text_WR-PPR.txt",
+  TEPPR: "https://s3-us-west-1.amazonaws.com/fftiers/out/text_TE-PPR.txt",
+  DST: "https://s3-us-west-1.amazonaws.com/fftiers/out/text_DST.txt",
+  RBPPR: "https://s3-us-west-1.amazonaws.com/fftiers/out/text_RB-PPR.txt"
+  // "RB-HALF-PPR":
+  //   "https://s3-us-west-1.amazonaws.com/fftiers/out/text_RB-HALF.txt"
+};
 
-  // TODO scrape all pages
-  // TODO support PPR/HALF-PPR, NON-PPR etc
-  // TODO by default just use PPR until build more sophisticated
-  const pages = {
-    QB: "https://s3-us-west-1.amazonaws.com/fftiers/out/text_QB.txt",
-    RB: "https://s3-us-west-1.amazonaws.com/fftiers/out/text_RB.txt",
-    RBPPR: "https://s3-us-west-1.amazonaws.com/fftiers/out/text_RB-PPR.txt",
-    "RB-HALF-PPR":
-      "https://s3-us-west-1.amazonaws.com/fftiers/out/text_RB-HALF.txt"
-  };
-
-  // TODO fetch other positions
-  await page.goto(Pages.QB);
-
-  const results = await page.evaluate(() => {
-    const players = [];
-
-    var allData = document.getElementsByTagName("pre")[0].innerText;
-    var tiers = allData.trim().split("\n");
-
-    for (let i = 0; i < tiers.length; i++) {
-      const currentTier = tiers[i].match(/Tier ([0-9])\:/)[1];
-      const playersInTier = tiers[i].replace(/(Tier [0-9]\:)/, "").split(",");
-
-      for (let j = 0; j < playersInTier.length; j++) {
-        players.push({
-          tier: currentTier,
-          rank: players.length + 1,
-          name: playersInTier[j].trim()
-        });
-      }
+async function scrapePosition(position) {
+  const data = await rp(Pages[position]);
+  const players = [];
+  const tiers = data.trim().split("\n");
+  for (let i = 0; i < tiers.length; i++) {
+    const currentTier = tiers[i].match(/Tier ([0-9]+)\:/)[1];
+    const playersInTier = tiers[i].replace(/(Tier [0-9]+\:)/, "").split(",");
+    for (let j = 0; j < playersInTier.length; j++) {
+      players.push({
+        tier: currentTier,
+        rank: players.length + 1,
+        name: playersInTier[j].trim()
+      });
     }
+  }
+  return players;
+}
 
-    return players;
-  });
+async function getSomeData() {
+  const collection = {};
+  for (const position of Object.keys(Pages)) {
+    const players = await scrapePosition(position);
+    collection[position] = players;
+  }
 
-  console.log(JSON.stringify({ [Pages.QB]: results }));
+  console.log(JSON.stringify(collection));
+}
 
-  // TODO save to file
-  // TODO firebase
+getSomeData();
 
-  await browser.close();
-})();
+// TODO save to file
+// TODO firebase
