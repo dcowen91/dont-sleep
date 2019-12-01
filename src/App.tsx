ebc412d5b81c87ce421e95b43d85c05e02740fba
Drew Owen <dcowen91@gmail.com>
2019-12-01 13:34:11 -0800

    add base evergreen ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,4 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Hello } from "./components/Hello";
 
-ReactDOM.render(
-  <Hello compiler="TypeScript" framework="React" />,
-  document.getElementById("example")
-);
+ReactDOM.render(<Hello />, document.getElementById("example"));
--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,11 +1,56 @@
 import * as React from "react";
-export interface HelloProps {
-  compiler: string;
-  framework: string;
-}
+import { TextInputField, Pane, Heading, Text, Button } from "evergreen-ui";
 
-export const Hello = (props: HelloProps) => (
-  <h1>
-    Hello from {props.compiler} and {props.framework}!
-  </h1>
-);
+export const Hello = () => {
+  const [leagueId, setLeagueId] = React.useState("");
+  const [userId, setuserId] = React.useState("");
+
+  return (
+    <>
+      <Pane
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        flexDirection="column"
+      >
+        <Heading>Don't Sleep</Heading>
+        <Text>A tool to help power up your fantasy football teams</Text>
+      </Pane>
+      <Pane
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        margin={15}
+      >
+        <TextInputField
+          marginRight={15}
+          label="league id"
+          value={leagueId}
+          onChange={(e: any) => {
+            setLeagueId(e.target.value);
+          }}
+        />
+        <TextInputField
+          marginRight={15}
+          label="user id"
+          value={userId}
+          onChange={(e: any) => {
+            setuserId(e.target.value);
+          }}
+        />
+        <Button
+          appearance="primary"
+          onClick={() => {
+            console.log("CLICKED");
+            if (leagueId && userId) {
+              const url = `https://us-central1-dont-sleep-92c89.cloudfunctions.net/fetchRosters?leagueId=${leagueId}&userId=${userId}`;
+              fetch(url).then(res => console.log(res));
+            }
+          }}
+        >
+          Go!
+        </Button>
+      </Pane>
+    </>
+  );
+};
