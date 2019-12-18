094566f657e8c967d9435faa634ee4125f03b4a1
Drew Owen <dcowen91@gmail.com>
2019-12-18 12:09:25 -0800

    basic team list view

--- src/components/LeagueView.tsx
+++ src/components/LeagueView.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
-import { Pane, Text, Heading } from "evergreen-ui";
+import { Pane, Text, Heading, Card, Image, Avatar } from "evergreen-ui";
 import { useParams } from "react-router-dom";
 
 interface ITeam {
   user_id: string;
   league_id: string;
   display_name: string;
+  avatar?: string;
   metadata: {
     team_name: string;
   };
@@ -22,14 +23,52 @@ export const LeagueView = () => {
       .then(data => setTeamsData(data));
   }, [leagueId]);
 
+  // TODO fix how this renders
   return (
-    <Pane>
+    <Pane
+      marginTop={30}
+      display="flex"
+      alignItems="flex-start"
+      justifyContent="center"
+      flexWrap="wrap"
+    >
       {teams.map(team => {
         return (
-          <Pane>
-            <Heading>{team.metadata.team_name}</Heading>
-            <Text>{team.display_name}</Text>
-          </Pane>
+          <Card
+            key={team.user_id}
+            display="flex"
+            justifyContent="start"
+            alignItems="start"
+            flexDirection="column"
+            padding={8}
+            margin={8}
+            flexWrap="wrap"
+            background="tint2"
+            borderRadius={5}
+            border
+            elevation={1}
+            width={200}
+            minHeight={100}
+          >
+            <Heading>{team.display_name}</Heading>
+            <Text>{team.metadata.team_name}</Text>
+            {team.avatar ? (
+              <Image
+                height={30}
+                width={30}
+                borderRadius="50%"
+                backgroundSize="contain"
+                src={`https://sleepercdn.com/avatars/thumbs/${team.avatar}`}
+              />
+            ) : (
+              <Avatar
+                isSolid
+                name={team.display_name}
+                color="purple"
+                size={30}
+              />
+            )}
+          </Card>
         );
       })}
     </Pane>
